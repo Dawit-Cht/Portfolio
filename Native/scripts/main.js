31b55f264da4d6310355680933c92d4623015b17
dwait-chu <dawit.cht@gmail.com>
2022-03-05 19:50:12 +0700

    page structure css

---
+++ Native/scripts/main.js
@@ -0,0 +1,15 @@
+/*
+React Lib Must learn
+React
+React Hooks (ไม่ใช่ lib แต่เป็น concept ที่ควรเข้าใจ)
+React Router
+Next.js
+Axios (ยิง API)
+Dayjs (จัดการเรื่องเวลาและวันที่)
+Redux, Redux-saga, Redux-thunk (State management)
+MUI/Boostrap (Component library)
+Emotion/Tailwind (CSS Framework)
+*/ 
+function showDetailed(){
+    alert("It was clicked")
+}
